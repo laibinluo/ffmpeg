0070a6e4494bd963406489421548d12fcec17ebc
bingluo <bingluo@ijidou.com>
2016-06-07 11:42:54 +0800

    init ffmpeg

---
+++ obj/local/armeabi/objs-debug/hello-jni/hello-jni.o.d
@@ -0,0 +1,191 @@
+obj/local/armeabi/objs-debug/hello-jni/hello-jni.o: jni/hello-jni.c \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/string.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/cdefs.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/android/api-level.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/malloc.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/jni.h \
+ jni/include/libavcodec/avcodec.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/errno.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/errno.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm/errno.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm-generic/errno.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm-generic/errno-base.h \
+ jni/include/libavutil/samplefmt.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/stdint.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/_types.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/_types.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/_wchar_limits.h \
+ jni/include/libavutil/avutil.h jni/include/libavutil/common.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/inttypes.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/limits.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/limits.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/limits.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/internal_types.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/limits.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/syslimits.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm/page.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/math.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/types.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/posix_types.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/stddef.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/compiler.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm/posix_types.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm/types.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/types.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/kernel.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/stdio.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/stdlib.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/alloca.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/strings.h \
+ D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/memory.h \
+ jni/include/libavutil/attributes.h jni/include/libavutil/macros.h \
+ jni/include/libavutil/version.h jni/include/libavutil/avconfig.h \
+ jni/include/libavutil/mem.h jni/include/libavutil/error.h \
+ jni/include/libavutil/rational.h jni/include/libavutil/mathematics.h \
+ jni/include/libavutil/intfloat.h jni/include/libavutil/log.h \
+ jni/include/libavutil/pixfmt.h jni/include/libavutil/attributes.h \
+ jni/include/libavutil/avutil.h jni/include/libavutil/buffer.h \
+ jni/include/libavutil/cpu.h jni/include/libavutil/channel_layout.h \
+ jni/include/libavutil/dict.h jni/include/libavutil/frame.h \
+ jni/include/libavutil/buffer.h jni/include/libavutil/dict.h \
+ jni/include/libavutil/samplefmt.h jni/include/libavutil/log.h \
+ jni/include/libavutil/pixfmt.h jni/include/libavutil/rational.h \
+ jni/include/libavcodec/version.h jni/include/libavutil/version.h
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/string.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/cdefs.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/android/api-level.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/malloc.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/jni.h:
+
+jni/include/libavcodec/avcodec.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/errno.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/errno.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm/errno.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm-generic/errno.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm-generic/errno-base.h:
+
+jni/include/libavutil/samplefmt.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/stdint.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/_types.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/_types.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/_wchar_limits.h:
+
+jni/include/libavutil/avutil.h:
+
+jni/include/libavutil/common.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/inttypes.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/limits.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/limits.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/limits.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/internal_types.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/limits.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/syslimits.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm/page.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/math.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/types.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/posix_types.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/stddef.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/compiler.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm/posix_types.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm/types.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/types.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/kernel.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/stdio.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/stdlib.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/alloca.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/strings.h:
+
+D:/Tools/ndk/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/memory.h:
+
+jni/include/libavutil/attributes.h:
+
+jni/include/libavutil/macros.h:
+
+jni/include/libavutil/version.h:
+
+jni/include/libavutil/avconfig.h:
+
+jni/include/libavutil/mem.h:
+
+jni/include/libavutil/error.h:
+
+jni/include/libavutil/rational.h:
+
+jni/include/libavutil/mathematics.h:
+
+jni/include/libavutil/intfloat.h:
+
+jni/include/libavutil/log.h:
+
+jni/include/libavutil/pixfmt.h:
+
+jni/include/libavutil/attributes.h:
+
+jni/include/libavutil/avutil.h:
+
+jni/include/libavutil/buffer.h:
+
+jni/include/libavutil/cpu.h:
+
+jni/include/libavutil/channel_layout.h:
+
+jni/include/libavutil/dict.h:
+
+jni/include/libavutil/frame.h:
+
+jni/include/libavutil/buffer.h:
+
+jni/include/libavutil/dict.h:
+
+jni/include/libavutil/samplefmt.h:
+
+jni/include/libavutil/log.h:
+
+jni/include/libavutil/pixfmt.h:
+
+jni/include/libavutil/rational.h:
+
+jni/include/libavcodec/version.h:
+
+jni/include/libavutil/version.h:
